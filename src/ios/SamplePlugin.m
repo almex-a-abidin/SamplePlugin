f8a7677eed37a998767b33a5fb11b9431dc9c6d1
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-15 13:21:10 +0800

    Update SamplePlugin.m

--- src/ios/SamplePlugin.m
+++ src/ios/SamplePlugin.m
@@ -28,15 +28,16 @@
 // セッション開始メソッド
 - (void)beginScan:(CDVInvokedUrlCommand*)command {
     // セッションの作成
-    self.session = [[NFCTagReaderSession new]
-                initWithPollingOption: (NFCPollingISO14443 | NFCPollingISO15693)
-                delegate: self
-                queue:dispatch_get_main_queue()
-    ];
-    // アラートメッセージの設定
-    self.session.alertMessage = @"かざしてください";
-    // セッション開始
-    [self.session beginSession];
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 
