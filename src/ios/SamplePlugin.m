3d7a009cef5024a6cebb282c1ed1a334668aa9fa
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-15 13:24:21 +0800

    update

--- src/ios/SamplePlugin.m
+++ src/ios/SamplePlugin.m
@@ -26,18 +26,17 @@
 }
 
 // セッション開始メソッド
-- (void)beginScan:(CDVInvokedUrlCommand*)command {
+- (void)beginScan {
     // セッションの作成
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    // self.session = [[NFCTagReaderSession new]
+    //             initWithPollingOption: (NFCPollingISO14443 | NFCPollingISO15693)
+    //             delegate: self
+    //             queue:dispatch_get_main_queue()
+    // ];
+    // // アラートメッセージの設定
+    // self.session.alertMessage = @"かざしてください";
+    // // セッション開始
+    // [self.session beginSession];
 }
 
 
--- www/SamplePlugin.js
+++ www/SamplePlugin.js
@@ -6,8 +6,8 @@ SamplePlugin.prototype.coolMethod = function (arg0, success, error) {
     exec(success, error, 'SamplePlugin', 'coolMethod', [arg0]);
 };
 
-SamplePlugin.prototype.beginScan = function (arg0, success, error) {
-    exec(success, error, 'SamplePlugin', 'beginScan', [arg0]);
+SamplePlugin.prototype.beginScan = function () {
+    exec(null, null, 'SamplePlugin', 'beginScan', []);
 };
 
 if(!window.plugins)
