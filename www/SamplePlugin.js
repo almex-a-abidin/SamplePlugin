b6e138287a1a2964c01c0ade4c0eab6e2ea82175
artjohn29 <104538286+artjohn29@users.noreply.github.com>
2022-09-15 11:42:40 +0800

    update

--- www/SamplePlugin.js
+++ www/SamplePlugin.js
@@ -1,5 +1,17 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+function SamplePlugin() {};
+
+SamplePlugin.prototype.coolMethod = function (arg0, success, error) {
     exec(success, error, 'SamplePlugin', 'coolMethod', [arg0]);
 };
+
+
+if(!window.plugins)
+    window.plugins = {};
+
+if (!window.plugins.NfcSessionAtOBJC)
+    window.plugins.SamplePlugin = new SamplePlugin();
+
+if (typeof module != 'undefined' && module.exports)
+    module.exports = SamplePlugin;
